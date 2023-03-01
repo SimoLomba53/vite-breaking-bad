998d5f0d6abef37c50b8df68377fbd06ea11292a
SimoLomba53 <simolomba253@gmail.com>
2023-03-01 15:32:59 +0100

    struttura base

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+import MainPart from './components/MainPart.vue';
+import axios from "axios";
+
+export default {
+  components: { MainPart },
+
+
+  created() {
+    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0").then((response) => {
+      console.log(response);
+    })
+  },
+};
+</script>
+
+<template>
+  <MainPart />
+</template>
+
+<style scoped></style>
